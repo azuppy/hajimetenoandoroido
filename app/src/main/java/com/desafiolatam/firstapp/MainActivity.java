7160adaee47a681987d3d414575413d2ee6efb97
Azuppy <ale.lermanda1503@gmail.com>
2020-02-21 16:43:50 -0300

    Agreguè recordatorio del desafìo en AndroidManifest.xml

--- app/src/main/java/com/desafiolatam/firstapp/MainActivity.java
+++ app/src/main/java/com/desafiolatam/firstapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.desafiolatam.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView miImagen = findViewById(R.id.miImagen);
+
+        Picasso.get().load("https://i.imgur.com/3Urqxer.png").into(miImagen);
+
+        Button mibotoncito = findViewById(R.id.button01);
+
+
+        mibotoncito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "QUIERO UN VODKA AHORA", Toast.LENGTH_SHORT).show();
+
+                intentExample();
+            }
+        });
+
+    }
+
+    private void intentExample(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
